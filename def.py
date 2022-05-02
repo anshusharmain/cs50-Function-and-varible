fb8492b087913dd690b03672b804a1871b78c981
Anshu Sharma <77799051+anshusharmavats@users.noreply.github.com>
2022-05-02 15:49:19 +0530

    Add files via upload

---
+++ def.py
@@ -0,0 +1,32 @@
+'''
+#I am defing the keyword using def
+def hello():
+    print("hello")
+name = input("WHat's your name?")
+hello()
+print(name)
+'''
+'''
+def hello(to):
+    print("hello", to)
+name = input("WHat's your name?")
+hello(name)#i am taking single parameter as input So we see username and what we define
+#print(name)'''
+'''
+def hello(to="world"):#we define or put value on line also
+    print("hello", to)
+name = input("WHat's your name?")
+hello(name)'''
+#we use this code to define some for it and we just write name of def like in this we use main and we can use this anywhere
+def main():
+    name = input("WHat's your name?")
+    hello(name)
+'''    
+def hello(to="world"):
+    print("hello", to)
+'''
+def hello():
+    print("hello", name)
+
+main()
+
